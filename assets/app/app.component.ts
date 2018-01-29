65a1e94f04d7fe25c9d723762a51c6f0acf28629
VarmaSANJAY <sanjay.varma@relevancelab.com>
2018-01-29 22:12:25 +0530

    Using property binding

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Message} from "./messages/message.model";
 
 @Component({
     selector: 'my-app',
@@ -6,8 +7,5 @@ import { Component } from '@angular/core';
 
 })
 export class AppComponent {
-    message = {
-        content: 'A message',
-        author: 'Rahul'
-    }
+    message: Message = new Message('Some message', 'San')
 }
