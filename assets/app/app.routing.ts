e3084cdbe41b000a7b41f6e8e76b8535d8c18743
VarmaSANJAY <sanjay.varma@relevancelab.com>
2018-02-17 20:11:17 +0530

    Setting up routing

---
+++ assets/app/app.routing.ts
@@ -0,0 +1,13 @@
+import {RouterModule, Routes} from "@angular/router";
+import {MessagesComponent} from "./messages/messages.component";
+import {AuthenticationComponent} from "./auth/authentication.component";
+
+const APP_ROUTES: Routes = [
+    { path: '', redirectTo: '/messages', pathMatch: 'full'},
+    { path: 'auth', component: AuthenticationComponent},
+    { path: 'messages', component: MessagesComponent}
+];
+
+
+
+export const routing = RouterModule.forRoot(APP_ROUTES);
