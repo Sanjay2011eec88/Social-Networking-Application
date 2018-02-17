e2aadfb0c0bb05ba2b71103b74676faf20b05006
VarmaSANJAY <sanjay.varma@relevancelab.com>
2018-02-17 19:23:07 +0530

    Adding Message with a Template driven form

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 
 import {MessageService} from "./message.service";
 import {Message} from "./message.model";
+import {NgForm} from "@angular/forms";
 
 @Component({
     selector: 'app-message-input',
@@ -13,8 +14,10 @@ export class MessageInputComponent {
 
     }
 
-    onSave(value: string) {
-        const message = new Message(value, 'San');
+    onSubmit(form :NgForm) {
+        console.log(form);
+        const message = new Message(form.value.content, 'San');
         this.messageService.addMessage(message);
+        form.resetForm();
     }
 }
