3fc17e1b24f10e66f116d583e49436c27bf2d283
VarmaSANJAY <sanjay.varma@relevancelab.com>
2018-02-17 23:00:05 +0530

    Adding child routes

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -1,10 +1,11 @@
 import {RouterModule, Routes} from "@angular/router";
 import {MessagesComponent} from "./messages/messages.component";
 import {AuthenticationComponent} from "./auth/authentication.component";
+import {AUTH_ROUTES} from "./auth/auth.routes";
 
 const APP_ROUTES: Routes = [
     { path: '', redirectTo: '/messages', pathMatch: 'full'},
-    { path: 'auth', component: AuthenticationComponent},
+    { path: 'auth', component: AuthenticationComponent, children:AUTH_ROUTES },
     { path: 'messages', component: MessagesComponent}
 ];
 
