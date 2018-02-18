195edb5c30d06d7426393c88b9a4465fb7ee33d4
VarmaSANJAY <sanjay.varma@relevancelab.com>
2018-02-18 17:54:44 +0530

    Saving Messages wiring up frontend and backend

--- routes/messages.js
+++ routes/messages.js
@@ -7,7 +7,7 @@ router.post('/', function (req, res, next) {
     var message = new Message({
        content: req.body.content,
     });
-    
+
     message.save(function (err,result) {
         if(err){
             return res.status(500).json({
