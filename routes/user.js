6a7b686076e776d6f7444113d12f9cfe5692f0fa
VarmaSANJAY <sanjay.varma@relevacelab.com>
2018-03-03 23:05:15 +0530

    Backend Sign in

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var User = require('../models/user');
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 router.post('/', function (req, res, next) {
     var user = new User({
@@ -9,7 +10,7 @@ router.post('/', function (req, res, next) {
         lastName: req.body.lastName,
         password: bcrypt.hashSync(req.body.password, 10),
         email: req.body.email
-    })
+    });
 
     user.save(function (err, result) {
         if(err){
@@ -26,5 +27,34 @@ router.post('/', function (req, res, next) {
     })
 });
 
+router.post('/signin',function (req, res, next) {
+    User.findOne({email: req.body.email}, function (err, user) {
+        if(err){
+            return res.status(500).json({
+                title:"An error occured",
+                error: err
+            });
+        }
+        if (!user) {
+            return res.status(401).json({
+                title:"No user found",
+                error: {message: 'Invalid login credentilas'}
+            })
+        }
+        if (!bcrypt.compareSync(req.body.password, user.password)) {
+            return res.status(401).json({
+                title:"No user found",
+                error: {message: 'Invalid login credentilas'}
+            })
+        }
+        var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+        res.status(200).json({
+            message:'Successfully logged in',
+            token: token,
+            userId: user._id
+        })
+
+    });
+});
 
 module.exports = router;
