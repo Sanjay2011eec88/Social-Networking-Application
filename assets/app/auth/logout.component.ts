f6b8a4f82474ed1c60ded93c5b0313775f91aa29
VarmaSANJAY <sanjay.varma@relevacelab.com>
2018-03-16 23:22:52 +0530

    Logging users out

--- assets/app/auth/logout.component.ts
+++ assets/app/auth/logout.component.ts
@@ -1,5 +1,7 @@
 
 import {Component} from "@angular/core";
+import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-logout',
@@ -10,7 +12,10 @@ import {Component} from "@angular/core";
     `
 })
 export class LogoutComponent{
-    onLogout(){
+    constructor(private authService: AuthService, private router: Router) {}
 
+    onLogout(){
+        this.authService .logout();
+        this.router.navigate(['/auth', 'signin']);
     }
 }
